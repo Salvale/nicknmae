4d81f0577d7b552b6d5401be510d1164a29e1f4a
Salvale <45802318+Salvale@users.noreply.github.com>
2021-02-10 10:50:15 -0700

    Update Program.cs

--- niceonebuddy/ConsoleApp1/Program.cs
+++ niceonebuddy/ConsoleApp1/Program.cs
@@ -10,12 +10,13 @@ namespace ConsoleApp1
     {
         static class global
         {
+            //initialize default nickname list
             public static List<string> nicks = new List<string> {"Snake","Ocelot","Raven","Wolf","Octopus","Mantis"};
         }
         
         static void Main()
         {
-
+            //Gets first and last names
             Console.WriteLine("Enter your first name:");
             string Name1 = Console.ReadLine();
             Console.WriteLine("Enter your last name:");
@@ -25,6 +26,7 @@ namespace ConsoleApp1
         }
         static void Draw(string Name1, string Name2)
         {
+            //write all relevant info, automatically begin with a random nickname
             Console.WriteLine("Hello, " + Name1 + " '" + Randy() + "' " + Name2);
             Console.WriteLine("Enter an input to try a command.");
             Console.WriteLine("1: Change Name");
@@ -33,28 +35,34 @@ namespace ConsoleApp1
             Console.WriteLine("4: Add a possible nickname");
             Console.WriteLine("5: Remove a nickname");
             Console.WriteLine("6. Close");
+            //get and scan command
             int Input = Convert.ToInt32(Console.ReadLine());
             if (Input == 1)
             {
+                //re-run main to get first and last name all over again
                 Main();
             } else if (Input == 2)
             {
+                //restart the current function for a new random nickname
                 Console.Clear();
                 Randy();
                 Draw(Name1, Name2);
             } else if (Input == 3)
             {
+                //Clear console and write all available nicknames
                 Console.Clear();
                 showAll();
                 Draw(Name1, Name2);
             } else if (Input == 4)
             {
+                //get input and add to the list of possible nicknames
                 Console.Write("What nickname would you like to add? ");
                 global.nicks.Add(Console.ReadLine());
                 Console.Clear();
                 Draw(Name1, Name2);
             } else if (Input == 5)
             {
+                //prevent all values from being removed
                 if (global.nicks.Count == 1)
                 {
                     Console.Clear();
@@ -63,17 +71,20 @@ namespace ConsoleApp1
                 }
                 else
                 {
-                    
+                   //get input for list item to remove
                     Console.WriteLine("Which one would you like to remove?");
                     string removed = Console.ReadLine();
+                    //Check if input is valid
                     if (global.nicks.Contains(removed))
                     {
+                        //valid
                         global.nicks.Remove(removed);
                         Console.Clear();
                         Console.WriteLine(removed + " removed!");
                     }
                     else
                     {
+                        //invalid
                         Console.Clear();
                         Console.WriteLine(removed + " not found!");
                     }
@@ -81,20 +92,24 @@ namespace ConsoleApp1
                 } 
             } else if (Input == 6)
             {
+                //kill the program
                 System.Environment.Exit(0);
             } else
             {
+                //failsafe for invalid command
                 Console.WriteLine("not a valid prompt");
             }
         }
         static string Randy()
         {
+            //get random list item
             System.Random random = new System.Random();
             int rand = random.Next(global.nicks.Count);
             return global.nicks[rand];
         }
         static void showAll()
         {
+            //Put all the list items together into a string
                 string allNicks = String.Join(", ", global.nicks);
                 Console.WriteLine("The list of all nicknames is " + allNicks);
         }
